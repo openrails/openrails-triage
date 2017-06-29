2127ed08313a187429dabb034154d5fcc2b54e94
James Ross <git@james-ross.co.uk>
2017-06-29 19:57:25 +0100

    dotnet new console app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Open_Rails_Roadmap_bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
