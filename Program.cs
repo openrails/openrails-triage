a1bbf52771ebbf8059b9e45d46a963ac9d642024
James Ross <git@james-ross.co.uk>
2017-06-29 20:04:04 +0100

    Add basic command line and configuration system

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
 
 namespace Open_Rails_Roadmap_bot
 {
@@ -6,7 +8,34 @@ namespace Open_Rails_Roadmap_bot
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var config = new CommandLineParser.Arguments.FileArgument('c', "config")
+            {
+                DefaultValue = new FileInfo("config.json")
+            };
+
+            var commandLineParser = new CommandLineParser.CommandLineParser()
+            {
+                Arguments = {
+                    config,
+                }
+            };
+
+            try
+            {
+                commandLineParser.ParseCommandLine(args);
+
+                Main(new ConfigurationBuilder()
+                    .AddJsonFile(config.Value.FullName, true)
+                    .Build());
+            }
+            catch (CommandLineParser.Exceptions.CommandLineException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        static void Main(IConfigurationRoot config)
+        {
         }
     }
 }
